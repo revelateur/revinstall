cbb0b3ff865a1d9f6c7659952dbe6b4066de6b87
Mathieu Menegain <mmenegain@revelateur.fr>
2021-09-20 10:37:48 +0200

    Finalisation de la commande d'installation pour wordpress

---
+++ src/General.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Revelateur\Revinstall\Console;
+
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Process\Process;
+
+class General extends Command
+{
+    /**
+     *
+     */
+    public function decoration(OutputInterface $output)
+    {
+        $output->write(PHP_EOL.'<fg=red>
+        ____  _______    _______   ________________    __    __ 
+        / __ \/ ____/ |  / /  _/ | / / ___/_  __/   |  / /   / / 
+       / /_/ / __/  | | / // //  |/ /\__ \ / / / /| | / /   / /  
+      / _, _/ /___  | |/ // // /|  /___/ // / / ___ |/ /___/ /___
+     /_/ |_/_____/  |___/___/_/ |_//____//_/ /_/  |_/_____/_____/</>'.PHP_EOL.PHP_EOL);
+    }
+
+    /**
+     * Question required params
+     */
+    protected function questionRequired(string $text, InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+
+        do {
+            $question = new Question($text);
+            $resp = $helper->ask($input, $output, $question);
+        } while (empty($resp));
+
+        return $resp;
+    }
+
+    /**
+     * Question params
+     */
+    protected function question(string $text, InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $question = new Question($text);
+        return $helper->ask($input, $output, $question);
+    }
+
+    /**
+     * Question params
+     */
+    protected function confirm(string $text, InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion($text, false);
+        return $helper->ask($input, $output, $question);
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        $composerPath = getcwd().'/composer.phar';
+
+        if (file_exists($composerPath)) {
+            return '"'.PHP_BINARY.'" '.$composerPath;
+        }
+
+        return 'composer';
+    }
+
+    /**
+     * Verify that the .env exist.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function verifyEnvExist()
+    {
+        if (!file_exists(getcwd().'/.env')) {
+            throw new RuntimeException('File .env not found');
+        }
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  array  $env
+     * @return \Symfony\Component\Process\Process
+     */
+    protected function runCommands($commands, InputInterface $input, OutputInterface $output, array $env = [])
+    {
+        if (! $output->isDecorated()) {
+            $commands = array_map(function ($value) {
+                if (substr($value, 0, 5) === 'chmod') {
+                    return $value;
+                }
+
+                return $value.' --no-ansi';
+            }, $commands);
+        }
+
+        if ($input->getOption('quiet')) {
+            $commands = array_map(function ($value) {
+                if (substr($value, 0, 5) === 'chmod') {
+                    return $value;
+                }
+
+                return $value.' --quiet';
+            }, $commands);
+        }
+
+        if (!empty($commands)) {
+            foreach ($commands as $command) {
+                $process = Process::fromShellCommandline($command, null, $env, null, null);
+                $output->writeln('<options=bold,underscore>'.$command.'</>');
+                $process->run();
+
+                if (!$process->isSuccessful()) {
+                    throw new ProcessFailedException($process);
+                }
+            }
+        } else {
+            die();
+        }
+
+        return $process;
+    }
+
+    public function runCommand($command, InputInterface $input, OutputInterface $output, array $env = [])
+    {
+        $process = Process::fromShellCommandline($command, null, $env, null, null);
+        
+        $output->writeln('<options=bold,underscore>'.$command.'</>');
+        $process->run();
+
+        return $process->getOutput();
+    }
+}
---
+++ src/WPCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Revelateur\Revinstall\Console;
+
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Dotenv\Dotenv;
+
+class WPCommand extends General
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('wp')
+            ->setDescription('Install and configure a Wordpress project');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->decoration($output);
+
+        $composer = $this->findComposer();
+
+        $this->verifyEnvExist();
+        $dotenv = Dotenv::createImmutable(getcwd());
+        $dotenv->load();
+
+        $this->createDatabase($input, $output);
+        $url = $this->questionRequired('Entrer le domaine à utiliser : ', $input, $output);
+        $title = $this->questionRequired('Entrer le titre du site : ', $input, $output);
+        $user = $this->questionRequired('Entrer le username de l\'admin : ', $input, $output);
+        $pass = $this->questionRequired('Entrer le mot de passe de l\'admin : ', $input, $output);
+        $mail = $this->questionRequired('Entrer l\'adresse mail de l\'admin : ', $input, $output);
+        $chown = $this->confirm('Chown le dossier uploads ? Laisser vide pour non : ', $input, $output);
+
+        chdir(getcwd());
+        $commands = [
+            $composer." u",
+            'wp core install --url='.$url.' --title='.$title.' --admin_user='.$user.' --admin_password='.$pass.' --admin_email='.$mail,
+            'wp theme activate '.$_ENV['WP_THEME'],
+            'wp language core activate fr_FR',
+            'wp plugin activate --all',
+            'wp menu create "Menu Principal"',
+            'wp menu location assign menu-principal primary',
+            'wp menu create "Pied de page"',
+            'wp menu location assign pied-de-page footer',
+            'wp post delete 1 --force',
+        ];
+
+        if (!empty($chown)) {
+            $commands[] = 'sudo chown www-data:www-data -R ./public/uploads';
+            $commands[] = 'sudo chmod 775 ./public/wp-config.php';
+        }
+        
+        $this->runCommands($commands, $input, $output);
+        $this->defineHomePage($input, $output);
+
+        $commands = [
+            'npm install',
+            'npm run build'
+        ];
+
+        if (($process = $this->runCommands($commands, $input, $output))->isSuccessful()) {
+            $output->writeln(PHP_EOL.'<comment>Application ready! Build something amazing.</comment>');
+        }
+
+        return $process->getExitCode();
+    }
+
+    /**
+     * Create database by .env file
+     */
+    protected function createDatabase(InputInterface $input, OutputInterface $output)
+    {
+        $commands = [
+            'mysql -h '.$_ENV['DB_HOST'].' -u '.$_ENV['DB_USER'].' -p'.$_ENV['DB_PASSWORD'].' -e "CREATE DATABASE IF NOT EXISTS '.$_ENV['DB_NAME'].'"'
+        ];
+
+        $this->runCommands($commands, $input, $output);
+    }
+
+    public function defineHomePage(InputInterface $input, OutputInterface $output)
+    {
+        // Create home page
+        $command = 'wp post create --post_type=page --post_title="Accueil" --post_status=publish';
+        $post = $this->runCommand($command, $input, $output);
+        preg_match('/(?<=post )(.*)(?=.)/', $post, $homeID);
+
+        // Definie home page in read option
+        $this->runCommands([
+            'wp option update show_on_front "page"',
+            'wp option update page_on_front "'.$homeID[0].'"',
+            'wp menu item add-post menu-principal '.$homeID[0],
+            'wp menu item add-post menu-principal 2'
+        ], $input, $output);
+    }
+}
